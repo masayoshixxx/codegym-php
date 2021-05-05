7e919aaf30c2c144f579ae415b62daf01fdb3c02
masayoshixxx <satomasayoshi.xxx@gmail.com>
2021-05-05 21:25:28 +0900

    整理アルゴリズムのバブルソートを参考に作成しました。

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,9 +2,17 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
+$length=count($array);
 
-}
+for($i=0;$i<$length-1;$i++){
+    for($j=0;$j<$length-1;$j++){
+      if($array[$j]>$array[$j+1]){
+        $tmp = $array[$j];
+        $array[$j] = $array[$j+1];
+        $array[$j+1] = $tmp;
+      }
+    }
+  }
 // 修正はここまで
 
 echo "<pre>";
