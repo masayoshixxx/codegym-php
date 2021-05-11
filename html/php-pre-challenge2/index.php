ba999d571ee98c332db7446c59aeac1a0bc03cfc
masayoshixxx <satomasayoshi.xxx@gmail.com>
2021-05-11 18:47:40 +0900

    インデントの調整と比較回数のカウント

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -4,17 +4,19 @@ $array = explode(',', $_GET['array']);
 // 修正はここから
 $length = count($array);
 
-for($i = 0; $i < $length - 1; $i++) {
-    for($j = 1; $j < $length - $i; $j++) {
-      if($array[$j] < $array[$j - 1]) {
-        $tmp = $array[$j];
-        $array[$j] = $array[$j - 1];
-        $array[$j - 1] = $tmp;
-      }
+for ($i = 0; $i < $length - 1; $i++) {
+  for ($j = 1; $j < $length - $i; $j++) {
+    $cnt++;
+    if ($array[$j] < $array[$j - 1]) {
+      $tmp = $array[$j];
+      $array[$j] = $array[$j - 1];
+      $array[$j - 1] = $tmp;
     }
   }
+}
 // 修正はここまで
 
 echo "<pre>";
 print_r($array);
+print_r("比較回数: $cnt 回");
 echo "</pre>";
